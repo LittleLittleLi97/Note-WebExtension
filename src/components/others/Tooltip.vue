4438ab7c3e014cd46f730057d55c3089bd624f9f
Ivan <chenyifei9@live.com>
2022-08-30 21:04:44 +0800

    add tooltip unfinished

---
+++ src/components/others/Tooltip.vue
@@ -0,0 +1,49 @@
+<template>
+    <div
+        @mouseenter="enter" 
+        @mouseleave="leave" 
+        @mousemove="move">
+        鼠标触碰元素
+    </div>
+ 
+ 
+<div v-show="popUpShow" class="hover_con" :style="positionStyle">
+悬浮框
+</div>
+</template>
+
+<script>
+export default {
+    name: '',
+    data() {
+      return {
+        popUpShow:false,
+        
+        positionStyle:{top:'0px',left:'0px'}
+      }
+    },
+    methods: {
+      enter() {
+        this.popUpShow = true
+      },
+      leave() {
+        this.popUpShow = false
+      },
+      move(event) {
+        const x = event.pageX + 15 + 'px'
+        const y = event.pageY + 10 + 'px'
+        this.positionStyle = { top: y, left: x }  
+      }
+    }
+}
+</script>
+
+<style>
+.hover_con{
+    position: fixed;
+    max-width: 220px;
+    padding: 10px;
+    border: 1px solid #666;
+    background: #ccc;
+}
+</style>
